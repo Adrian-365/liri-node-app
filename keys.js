604c1b15c13a37f59194ea14c1706482f4fc64ff
Adrian Nalwalker <analwalker@planetfg.local>
2017-11-25 12:31:06 -0800

    itial commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'P2ZxJ51XUkmFyCycFeNZQ5dk4',
+  consumer_secret: 'K8SoeBDF5Uli3ACnFz8vHaNcZmXkeiSEHkxalCMLGCiHtgjycb',
+  access_token_key: '934271569676836864-tsg9EzED9GLv7mrnl07lDI8rCRcL5lv',
+  access_token_secret: 'Xq3fvYs3LC2v1wbNngEQAzFR0S6KAolNfSPFOc7umS2ml',
+}
+
+module.exports = twitterKeys;
